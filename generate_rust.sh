b4a9a48f59aa8219db0eaa540b7e1aba3f252bfc
John Paul Rutigliano <johnpaul122jpr@gmail.com>
2023-05-20 22:32:22 -0400

    fix package name

--- generate_rust.sh
+++ generate_rust.sh
@@ -1,6 +1,8 @@
 swagger_file='/tmp/swagger-fibermc-auto.json'
 output_dir='./rust'
 
-curl -sk https://localhost:5001/swagger/v1-pre/swagger.json > $swagger_file
-npx @openapitools/openapi-generator-cli generate -i $swagger_file  -g rust -o $output_dir
+curl -sk https://localhost:5001/swagger/v0/swagger.json > $swagger_file
+npx @openapitools/openapi-generator-cli generate -i $swagger_file  -g rust -o $output_dir \
+    --package-name 'fibermc-sdk' \
+#    --release-note 'extremely early alpha. quite unstable at present.' \
 
